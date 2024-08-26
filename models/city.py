b5086674c3ac5142da522139c4967ede872b007e
JoshuaAisien <joshaisien@gmail.com>
2024-08-26 18:38:47 -0100

    create_file

--- models/city.py
+++ models/city.py
@@ -2,11 +2,14 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel,Base
 from sqlalchemy import String, Column, ForeignKey
-
-
+from sqlalchemy.orm import relationship
+from models.place import Place
 class City(BaseModel, Base):
     """ The city class"""
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # relationships
+    places = relationship('Place', back_populates='cities', cascade="all, delete-orphan")
--- models/place.py
+++ models/place.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from models.engine.db_storage import DBStorage
+from models.review import Review
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = "places"
 
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    # Relationship with User and city
+    reviews = relationship("Review", back_populates="place", cascade="all, delete-orphan")
+    cities = relationship('City', back_populates='places')
+    user = relationship('User', back_populates='places')
+    @property
+    def review_list(self):
+        """  returns the list of Review instances with place_id equals to the current Place.id """
+        from models import storage
+        from models.review import Review
+        return [review_list for review_list in storage.all(Review).values() if review_list.place_id == self.id]
